fa52cdb1a17b7f37c80da6289f7f21828329e2a2
brittanymae01 <brittanymae01>
2019-12-05 15:20:40 -0800

    scoreboard created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Display from './components/Display'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      balls: 0,
+      strikes: 0
+    }
+  }
+
+  hitBall = () => {
+    this.setState({
+      balls: 0,
+      strikes: 0
+    })
+  }
+
+  foulBall = () => {
+    this.setState(prevState => {
+      if (prevState.strikes >= 2) {
+        return { strikes: this.state.strikes }
+      }
+      return { ...prevState, strikes: this.state.strikes + 1 }
+    })
+  }
+
+  addBalls = () => {
+    this.setState(prevState => {
+      if (prevState.balls >= 4) {
+        return { balls: 0 }
+      }
+      return { ...prevState, balls: prevState.balls + 1 }
+    })
+  }
+
+  addStrikes = () => {
+    this.setState(prevState => {
+      if (prevState.strikes >= 3) {
+        return { strikes: 0 }
+      }
+      return { ...prevState, strikes: prevState.strikes + 1 }
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        <Display strikes={this.state.strikes} balls={this.state.balls} />
+        <Dashboard balls={this.state.balls} strikes={this.state.strikes} addBalls={this.addBalls} addStrikes={this.addStrikes} foulBall={this.foulBall} hitBall={this.hitBall} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
